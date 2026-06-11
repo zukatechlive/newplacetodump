a951ee627d766031512f964814a2ee442c97e2eb
zukatechlive <jwiskewl75@gmail.com>
2026-06-11 13:36:47 -0500

    Create Tycoon.lua

---
+++ random/Tycoon.lua
@@ -0,0 +1,70 @@
+local GunSettings = game:GetService("ReplicatedStorage").GunSettings
+
+local guns = {
+	"AK-47",
+	"AA-12",
+	"AUG",
+	"AWP",
+	"Revolver",
+	"M1 Garand",
+	"Glock",
+	"LMG",
+	"Deagle",
+	"Crossbow",
+	"Cosmic MP7",
+	"Berreta",
+	"Raygun",
+}
+
+local overrides = {
+	DelayAfterFiring = 0,
+	LaserBeamStartupDelay = 0,
+	ReduceSelfDamageOnAirOnly = 999999,
+	CriticalDamageEnabled = 999999,
+	MeleeCriticalDamageMultiplier = 999999,
+	HeadshotHitmarker = 100,
+	ReloadTime = 0,
+	Lifesteal = 99999,
+	Spread = 0,
+	ChargingTime = 0,
+	HitIgnoreDelay = 0,
+	BurstRate = 0,
+	Recoil = 0,
+	LaserTrailDamage = 999999,
+	DelayBeforeFiring = 0,
+	AmmoPerMag = 999999,
+	Range = 90000,
+	TacticalReloadTime = 0,
+	SwitchTime = 0,
+	BounceDelay = 0,
+	AngleXMin = 0,
+	DamageThroughWall = 999999,
+	Accuracy = 0,
+}
+
+for _, gunName in ipairs(guns) do
+	local ok, result = pcall(function()
+		return require(GunSettings[gunName]["1"])
+	end)
+
+	if not ok then
+		warn(("patched! aint that shitty"):format(gunName, result))
+		continue
+	end
+
+	local mod = result
+
+	if setreadonly then
+		setreadonly(mod, false)
+	end
+
+	for key, value in pairs(overrides) do
+		mod[key] = value
+	end
+
+	if setreadonly then
+		setreadonly(mod, true)
+	end
+
+	print(("we're lit"):format(gunName))
+end
